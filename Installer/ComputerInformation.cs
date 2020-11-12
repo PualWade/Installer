deaa6989922035bbe02e02cd8d0eaf47fdf187e0
PaulWade <Alantant0032@yandex.ru>
2020-11-12 10:19:06 +0300

    Добавил класс (Сведения о компьютере)

---
+++ Installer/ComputerInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Installer
+{
+    class ComputerInformation
+    {
+        public string UserName { get; set; }
+        public string ComputerName { get; set; }
+        public string PathWindows { get; set; }
+        public string PathSystemFiles { get; set; }
+    }
+}
