26118ecf4af8982c7441e8214ae6c2a939f7aeac
PaulWade <Alantant0032@yandex.ru>
2020-11-12 13:26:21 +0300

    Добавил класс Sender для хеширования

---
+++ Installer/Sender.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Installer
+{
+    class Sender
+    {
+        RSAParameters rsaPubParams;
+        RSAParameters rsaPrivateParams;
+
+        public Sender()
+        {
+            RSACryptoServiceProvider rsaCSP = new RSACryptoServiceProvider();
+
+            //Generate public and private key data.
+            rsaPrivateParams = rsaCSP.ExportParameters(true);
+            rsaPubParams = rsaCSP.ExportParameters(false);
+        }
+
+        public RSAParameters PublicParameters
+        {
+            get
+            {
+                return rsaPubParams;
+            }
+        }
+
+        public byte[] HashAndSign(byte[] encrypted)
+        {
+            RSACryptoServiceProvider rsaCSP = new RSACryptoServiceProvider();
+            SHA1Managed hash = new SHA1Managed();
+            byte[] hashedData;
+
+            rsaCSP.ImportParameters(rsaPrivateParams);
+
+            hashedData = hash.ComputeHash(encrypted);
+            return rsaCSP.SignHash(hashedData, CryptoConfig.MapNameToOID("SHA1"));
+        }
+
+        
+        public byte[] EncryptData(RSAParameters rsaParams, byte[] toEncrypt)
+        {
+            RSACryptoServiceProvider rsaCSP = new RSACryptoServiceProvider();
+
+            rsaCSP.ImportParameters(rsaParams);
+            return rsaCSP.Encrypt(toEncrypt, false);
+        }
+    }
+}
