d620ad367d651d6598814dc268bb640b757d439b
PaulWade <Alantant0032@yandex.ru>
2020-11-12 13:05:52 +0300

    Дополнил класс ComputerInformation

--- Installer/ComputerInformation.cs
+++ Installer/ComputerInformation.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Installer
 {
@@ -12,12 +13,13 @@ namespace Installer
     {
         public static string UserName = Environment.UserName;
         public static string ComputerName = Environment.MachineName;
-        public static string PathWindows = "";
+        public static string PathWindows = Environment.CurrentDirectory;
         public static string PathSystemFiles = Environment.SystemDirectory;
-        public static int MouseButtons = System.Windows.Forms.SystemInformation.MouseButtons;
+        //public static int MouseButtons = System.Windows.Forms.SystemInformation.MouseButtons;
         public static Size PrimaryMonitorSize = System.Windows.Forms.SystemInformation.PrimaryMonitorSize;
         public static long RAM = Environment.WorkingSet;
-
         public static long Drive = new DriveInfo(Convert.ToString(Directory.GetCurrentDirectory()[0])).TotalSize;
+        public static string TypeKeyboard = Keyboard.PrimaryDevice.ToString();
+
     }
 }
